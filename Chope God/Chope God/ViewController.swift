934d3d24489176d6ae96d4ac2f832384f4309c52
Harsh Gadodia <harsh@u.nus.edu>
2018-01-27 22:25:40 +0800

    work on adding a button and new objects

--- Chope God/Chope God/ViewController.swift
+++ Chope God/Chope God/ViewController.swift
@@ -11,16 +11,85 @@ import SceneKit
 import ARKit
 import ARCL
 import CoreLocation
+import PopupDialog
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     var sceneLocationView = SceneLocationView()
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var addNewObjectButton: UIButton!
     
+    @IBAction func addNewObjectAction(_ sender: Any) {
+        let title = "Chope your seat!"
+        let message = "Choose your object to reserve your seat with"
+        
+        let popup = PopupDialog(title: title, message: message)
+        
+        // Create buttons
+        let buttonOne = CancelButton(title: "Dice") {
+            // Create a new scene
+            let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+            
+            if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                
+                diceNode.position = SCNVector3(
+//                    x: hitResult.worldTransform.columns.3.x,
+//                    y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+//                    z: hitResult.worldTransform.columns.3.z
+                )
+                
+                self.sceneView.scene.rootNode.addChildNode(diceNode)
+                
+            }
+        }
+        
+        // This button will not the dismiss the dialog
+        let buttonTwo = DefaultButton(title: "Starbucks") {
+            // Create a new scene
+            let antScene = SCNScene(named: "art.scnassets/StrBucks.scn")!
+            
+            if let antNode = antScene.rootNode.childNode(withName: "ant", recursively: true) {
+                
+                antNode.position = SCNVector3(
+//                    x: hitResult.worldTransform.columns.3.x,
+//                    y: hitResult.worldTransform.columns.3.y + antNode.boundingSphere.radius,
+//                    z: hitResult.worldTransform.columns.3.z
+                )
+                
+                self.sceneView.scene.rootNode.addChildNode(antNode)
+                
+            }
+            
+        }
+        
+        let buttonThree = DefaultButton(title: "Courage the Cowardly Dog", height: 60) {
+            // Create a new scene
+            let courageScene = SCNScene(named: "art.scnassets/courage_apply.scn")!
+            
+            if let courageNode = courageScene.rootNode.childNode(withName: "courage", recursively: true) {
+                
+                courageNode.position = SCNVector3(
+//                    x: hitResult.worldTransform.columns.3.x,
+//                    y: hitResult.worldTransform.columns.3.y + courageNode.boundingSphere.radius,
+//                    z: hitResult.worldTransform.columns.3.z
+                )
+                
+                self.sceneView.scene.rootNode.addChildNode(courageNode)
+                
+            }
+        }
+        
+        popup.addButtons([buttonOne, buttonTwo, buttonThree])
+        
+        self.present(popup, animated: true, completion: nil)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //view.bringSubview(toFront: addNewObjectButton)
+
         sceneLocationView.run()
         view.addSubview(sceneLocationView)
         
@@ -64,6 +133,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            let planeAnchor = anchor as! ARPlaneAnchor
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x) , height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            
+            //Add child node to plane
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
+    }
 
     // MARK: - ARSCNViewDelegate
     
@@ -75,6 +166,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first {
+       
+            }
+        }
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -90,4 +193,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    // MARK: - Saving plane info
+//    func makePlane(from planeInfo: PlaneInfo) -> SCNNode { // call this when you place content
+//        let extent = planeInfo.extent
+//        let center = float4(planeInfo.center, 1) * planeInfo.transform
+//        // we're positioning content in world space, so center is now
+//        // an offset relative to transform
+//
+//        let plane = SCNPlane(width: CGFloat(extent.x), height: CGFloat(extent.z))
+//        let planeNode = SCNNode(geometry: plane)
+//        planeNode.eulerAngles.x = .pi / 2
+//        planeNode.simdPosition = center.xyz
+//
+//        return planeNode
+//    }
+    
+    // load the image chosen using the imageNumber
+//    func loadImageChosen(from planeInfo: PlaneInfo) {
+//
+//    }
 }
