3e42e9effe1e7865c0a2885bff900ebb75afca5d
Harsh Gadodia <harsh@u.nus.edu>
2018-01-27 14:27:43 +0800

    work on adding 3d image to plane

--- Chope God/Chope God/ViewController.swift
+++ Chope God/Chope God/ViewController.swift
@@ -14,8 +14,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var nodeModel:SCNNode!
+    let nodeName = "ant"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //show coordinate axis
+        sceneView.debugOptions = ARSCNDebugOptions.showWorldOrigin
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -23,15 +29,58 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        //smooths edges in rendered scenes
+        sceneView.antialiasingMode = .multisampling4X
+        
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
         
         // Add feature points
         sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        
+        let modelScene = SCNScene(named:
+            "art.scnassets/ant.scn")!
+        
+        nodeModel =  modelScene.rootNode.childNode(
+            withName: nodeName, recursively: true)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: sceneView)
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        let hitResults: [SCNHitTestResult]  =
+            sceneView.hitTest(location, options: hitTestOptions)
+        if let hit = hitResults.first {
+            if let node = getParent(hit.node) {
+                node.removeFromParentNode()
+                return
+            }
+        }
+        let hitResultsFeaturePoints: [ARHitTestResult] =
+            sceneView.hitTest(location, types: .featurePoint)
+        if let hit = hitResultsFeaturePoints.first {
+            sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+        }
+    }
+    
+    func getParent(_ nodeFound: SCNNode?) -> SCNNode? {
+        if let node = nodeFound {
+            if node.name == nodeName {
+                return node
+            } else if let parent = node.parent {
+                return getParent(parent)
+            }
+        }
+        return nil
+    }
+    
+
+    
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -67,25 +116,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //    }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if anchor is ARPlaneAnchor {
-            let planeAnchor = anchor as! ARPlaneAnchor
-            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x) , height: CGFloat(planeAnchor.extent.z))
-            
-            let planeNode = SCNNode()
-            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
-            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
-            
-            let gridMaterial = SCNMaterial()
-            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
-            
-            plane.materials = [gridMaterial]
-            planeNode.geometry = plane
-            
-            //Add child node to plane
-            node.addChildNode(planeNode)
-        } else {
-            return
+        
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+                
+                // Add model as a child of the node
+                node.addChildNode(modelClone)
+            }
         }
+//        if anchor is ARPlaneAnchor {
+//            let planeAnchor = anchor as! ARPlaneAnchor
+//            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x) , height: CGFloat(planeAnchor.extent.z))
+//
+//            let planeNode = SCNNode()
+//            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+//            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+//
+//            let gridMaterial = SCNMaterial()
+//            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+//
+//            plane.materials = [gridMaterial]
+//            planeNode.geometry = plane
+//
+//            //Add child node to plane
+//            node.addChildNode(planeNode)
+//        } else {
+//            return
+//        }
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
